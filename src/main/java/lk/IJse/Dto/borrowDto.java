e8c477b2177a0f944d48809151be565060853dbb
Shalom Hosheya <shalomhosheya37@gmail.com>
2024-03-10 21:23:09 +0530

    designing 3

---
+++ src/main/java/lk/IJse/Dto/borrowDto.java
@@ -0,0 +1,15 @@
+package lk.IJse.Dto;
+
+import java.time.LocalDateTime;
+
+public class borrowDto {
+    public static String bookId;
+    public static String name;
+    public static String title;
+
+    public static String branchADD;
+
+    public static LocalDateTime Bdate;
+    public static LocalDateTime HDate;
+
+}
